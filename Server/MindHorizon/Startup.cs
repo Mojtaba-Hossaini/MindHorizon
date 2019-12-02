aa4d98456342167f831585170ff8d5cf5a9667fb
MWW1987 <s.mojtaba1987@gmail.com>
2019-12-02 13:35:32 +0330

    Add admin layout

--- Server/MindHorizon/Startup.cs
+++ Server/MindHorizon/Startup.cs
@@ -23,7 +23,8 @@ namespace MindHorizon
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<MindHorizonDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MSSqlServer")));
+            services.AddDbContext<MindHorizonDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MSSqlServer")));
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,15 +35,19 @@ namespace MindHorizon
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
+
+            
+
+            
         }
     }
 }
